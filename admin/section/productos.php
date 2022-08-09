1c4c6402806f82c642a4069d0f2c164152ad8d9f
jusepthm <jusepth.dev@gmail.com>
2022-08-09 12:11:15 -0500

    validando datos

--- admin/section/productos.php
+++ admin/section/productos.php
@@ -1,5 +1,34 @@
 <?php include("../template/header.php"); ?>
+<?php 
 
+$txtID=(isset($_POST['txtID']))?$_POST['txtID']:"";
+$txtNombre=(isset($_POST['txtNombre']))?$_POST['txtNombre']:"";
+$txtImagen=(isset($_FILES['txtImagen']['name']))?$_FILES['txtImagen']['name']:"";
+$accion=(isset($_POST['accion']))?$_POST['accion']:"";
+
+echo $txtID."<br>";
+echo $txtNombre."<br>";
+echo $txtImagen."<br>";
+echo $accion."<br>";
+
+switch($accion){
+    case "":
+        echo "";
+        break;
+    case "":
+        echo "";
+        break;
+        case "":
+        echo "";
+        break;
+
+
+
+
+
+}
+
+?>
 <div class="col-md-5">
 
     <div class="card">
@@ -26,9 +55,9 @@
                 </div>
 
                 <div class="btn-group" role="group" aria-label="">
-                    <button type="button" name="accion" class="btn btn-success">Agregar</button>
-                    <button type="button" name="accion" class="btn btn-warning">Modificar</button>
-                    <button type="button" name="accion" class="btn btn-info">Cancelar</button>
+                    <button type="submit" name="accion" value="Agregar" class="btn btn-success">Agregar</button>
+                    <button type="submit" name="accion" value="Modificar" class="btn btn-warning">Modificar</button>
+                    <button type="submit" name="accion" value="Cancelar" class="btn btn-info">Cancelar</button>
                 </div>
             </form>
         </div>
